9f8fd8d26c2463a5655b4ab9ecc837f0a3abcff2
Zhao Jian <zhaojian90@u.nus.edu>
2018-11-26 09:34:19 +0800

    Add files via upload

--- src/Pre-_and_post-processing/FaceAlign-Resize-w-Padding.PyTorch/FaceAligner_utils.py
+++ src/Pre-_and_post-processing/FaceAlign-Resize-w-Padding.PyTorch/FaceAligner_utils.py
@@ -71,6 +71,72 @@ class FaceAligner:
         # return the aligned face
         return output
 
+class FaceAligner_VGGFace2:
+    def __init__(self, desiredLeftEye=(0.35, 0.35)):
+        self.desiredLeftEye = desiredLeftEye
+
+    def align(self, image, shape):
+        shape = shape
+
+        self.FaceWidth = image.shape[1]
+        self.FaceHeight = image.shape[0]
+
+        # simple hack ;)
+        if (len(shape) == 68):
+            # extract the left and right eye (x, y)-coordinates
+            (lStart, lEnd) = FACIAL_LANDMARKS_68_IDXS["left_eye"]
+            (rStart, rEnd) = FACIAL_LANDMARKS_68_IDXS["right_eye"]
+        else:
+            (lStart, lEnd) = FACIAL_LANDMARKS_5_IDXS["left_eye"]
+            (rStart, rEnd) = FACIAL_LANDMARKS_5_IDXS["right_eye"]
+
+        leftEyePts = shape[lStart:lEnd]
+        rightEyePts = shape[rStart:rEnd]
+
+        # compute the center of mass for each eye
+        leftEyeCenter = leftEyePts.mean(axis=0).astype("int")
+        rightEyeCenter = rightEyePts.mean(axis=0).astype("int")
+
+        # compute the angle between the eye centroids
+        dY = rightEyeCenter[1] - leftEyeCenter[1]
+        dX = rightEyeCenter[0] - leftEyeCenter[0]
+        angle = np.degrees(np.arctan2(dY, dX)) - 180
+
+        # compute the desired right eye x-coordinate based on the
+        # desired x-coordinate of the left eye
+        desiredRightEyeX = 1.0 - self.desiredLeftEye[0]
+
+        # determine the scale of the new resulting image by taking
+        # the ratio of the distance between eyes in the *current*
+        # image to the ratio of distance between eyes in the
+        # *desired* image
+        dist = np.sqrt((dX ** 2) + (dY ** 2))
+        desiredDist = (desiredRightEyeX - self.desiredLeftEye[0])
+        desiredDist *= self.FaceWidth
+        scale = desiredDist / dist
+
+        # compute center (x, y)-coordinates (i.e., the median point)
+        # between the two eyes in the input image
+        eyesCenter = ((leftEyeCenter[0] + rightEyeCenter[0]) // 2,
+                      (leftEyeCenter[1] + rightEyeCenter[1]) // 2)
+
+        # grab the rotation matrix for rotating and scaling the face
+        M = cv2.getRotationMatrix2D(eyesCenter, angle, scale)
+
+        # update the translation component of the matrix
+        tX = self.FaceWidth * 0.5
+        tY = self.FaceHeight * self.desiredLeftEye[1]
+        M[0, 2] += (tX - eyesCenter[0])
+        M[1, 2] += (tY - eyesCenter[1])
+
+        # apply the affine transformation
+        (w, h) = (self.FaceWidth, self.FaceHeight)
+        output = cv2.warpAffine(image, M, (w, h),
+                                flags=cv2.INTER_CUBIC)
+
+        # return the aligned face
+        return output
+
 class FaceResizer:
     def __init__(self, desiredFaceSize=224):
         self.desiredFaceSize = desiredFaceSize
