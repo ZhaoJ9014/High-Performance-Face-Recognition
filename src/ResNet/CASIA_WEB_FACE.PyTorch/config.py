2155197ee5673eaf64558b96e7c6af5e2eee99fd
Zhao Jian <zhaojian90@u.nus.edu>
2018-12-11 14:58:00 +0800

    Add files via upload

---
+++ src/ResNet/CASIA_WEB_FACE.PyTorch/config.py
@@ -0,0 +1,19 @@
+configurations = {
+    # ResNet from scratch with Adam
+    1: dict(
+        SEED=1337,
+        LR=0.001,
+        TRAIN_BATCH_SIZE=256,
+        VAL_BATCH_SIZE=1,
+        NUM_EPOCHS=30,
+        WEIGHT_DECAY=0.0005,
+        RGB_MEAN=[0.485, 0.456, 0.406],
+        RGB_STD=[0.229, 0.224, 0.225],
+        MODEL_NAME='ResNet50',
+        TRAIN_PATH='/home/zhaojian/zhaojian/DATA/CASIA_WEB_FACE_Aligned',
+        VAL_PATH='/home/zhaojian/zhaojian/DATA/lfw_Aligned',
+        PAIR_TEXT_PATH='/home/zhaojian/zhaojian/DATA/pairs.txt',
+        FILE_EXT='jpg',  # observe, no '.' before jpg
+        OPTIM='Adam',
+    ),
+}
