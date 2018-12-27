c8abbe26ce57ff0e74124e84eaa5f594acc4d3d4
Zhao Jian <zhaojian90@u.nus.edu>
2018-12-27 15:16:06 +0800

    Update face_align.py

--- src/Pre-_and_post-processing/FaceAlign-Resize-w-Padding.PyTorch/face_align.py
+++ src/Pre-_and_post-processing/FaceAlign-Resize-w-Padding.PyTorch/face_align.py
@@ -10,17 +10,16 @@ reference = get_reference_facial_points(default_square = True)
 if not os.path.isdir(dest_root):
     os.mkdir(dest_root)
 
-def align(img):
-    _, landmarks = detect_faces(img)
-    facial5points = [[landmarks[0][j], landmarks[0][j + 5]] for j in range(5)]
-    warped_face = warp_and_crop_face(np.array(img), facial5points, reference, crop_size=(112, 112)) # Modidy the crop_size according to your case 
-    return Image.fromarray(warped_face)
-
 for subfolder in os.listdir(source_root):
     if not os.path.isdir(os.path.join(dest_root, subfolder)):
         os.mkdir(os.path.join(dest_root, subfolder))
     for image_name in os.listdir(os.path.join(source_root, subfolder)):
             print("Processing\t{}".format(os.path.join(source_root, subfolder, image_name)))
             img = Image.open(os.path.join(source_root, subfolder, image_name))
-            img_warped = align(img)
+            _, landmarks = detect_faces(img)
+            if len(landmarks) == 0: # If the landmarks cannot be detected, the img will be discarded 
+                continue
+            facial5points = [[landmarks[0][j], landmarks[0][j + 5]] for j in range(5)]
+            warped_face = warp_and_crop_face(np.array(img), facial5points, reference, crop_size=(112, 112)) # Modidy the crop_size according to your case 
+            img_warped = Image.fromarray(warped_face)
             img_warped.save(os.path.join(dest_root, subfolder, image_name))
